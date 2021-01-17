732997dbb8a1414a92b50e76700a5081b5da23ca
COSMICALPHAgit <sudesh.kumarh2@gmail.com>
2021-01-17 19:22:57 +0530

    yc
    ggfy

--- commands/ban.js
+++ commands/ban.js
@@ -3,9 +3,10 @@ module.exports = {
     description: "This command ban a member!",
     execute(message, args){
 
-        if(message.member.roles.cache.has('776786219145232406')){
+        
         const member = message.mentions.users.first();
         if(member){
+            if(message.member.roles.cache.has('776786219145232406')){
             const memberTarget = message.guild.members.cache.get(member.id);
             memberTarget.ban();
             message.channel.send("User has been banned");
