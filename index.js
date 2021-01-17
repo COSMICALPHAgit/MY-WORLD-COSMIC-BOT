69c9d5f0319de019c5f72ce18e6cb57574040297
COSMICALPHAgit <sudesh.kumarh2@gmail.com>
2021-01-17 13:10:58 +0530

    wdwd
    wdwd

--- index.js
+++ index.js
@@ -35,8 +35,6 @@ client.on('message', message =>{
       .setColor('#fa0202')
       .setTitle('SERVER GOING-OFFLINE')                        
       .setFooter('ADMIN')
-      let message = message.join(" ");
-      message.delete();
       msg.channel.send(exampleEmbed)
     } 
     else  if (msg.content === '!tell') {
@@ -46,7 +44,14 @@ client.on('message', message =>{
       .setFooter('𝗕𝗬 𝗦𝗘𝗥𝗩𝗘𝗥 𝗢𝗪𝗡𝗘𝗥')
       msg.channel.send(exampleEmbed)
     }
-  
+    else if (msg.content.startsWith('!kick')) {
+      if (msg.mentions.users.size) {
+        const taggedUser = msg.mentions.users.first();
+        msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+      } else {
+        msg.reply('Please tag a valid user!');
+      }
+    }
   
 });
 
