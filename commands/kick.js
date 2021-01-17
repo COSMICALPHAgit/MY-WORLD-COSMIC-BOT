36ee16c4217d0aa4e232ccf037a46b833b8f325e
COSMICALPHAgit <sudesh.kumarh2@gmail.com>
2021-01-17 19:25:47 +0530

    wqed
    wefdd

--- commands/kick.js
+++ commands/kick.js
@@ -4,6 +4,7 @@ module.exports = {
     execute(message, args){
         const member = message.mentions.users.first();
         if(member){
+            if(message.member.roles.cache.has('776786219145232406')){
             const memberTarget = message.guild.members.cache.get(member.id);
             memberTarget.kick();
             message.channel.send("User has been kicked");
@@ -11,8 +12,9 @@ module.exports = {
 
            
 
-            message.channel.send('you could not kick that member');
+            message.channel.send('you you dont have the right permission from the server owner');
 
         }
     }
+}
 }
