d83af5ff925f4056bea2f668dd9eb330b769233b
COSMICALPHAgit <sudesh.kumarh2@gmail.com>
2021-01-19 21:48:08 +0530

    QWD
    QWSD

--- index.js
+++ index.js
@@ -73,24 +73,6 @@ client.on('message', message => {
     message.delete();
     client.commands.get('clear').execute(message, args);
   }
-  
-  else if(command === "unban"){
-    if(!msg.member.hasPermission("BAN_MEMBERS")) {
-      return msg.channel.send(`**${msg.author.username}**, You do not have perms to unban someone`)
-    }
-    
-    if(!msg.guild.me.hasPermission("BAN_MEMBERS")) {
-      return msg.channel.send(`**${msg.author.username}**, I do not have perms to unban someone`)
-    }
-    
-    let userID = args[0]
-      msg.guild.fetchBans().then(bans=> {
-      if(bans.size == 0) return 
-      let bUser = bans.find(b => b.user.id == userID)
-      if(!bUser) return
-      msg.guild.members.unban(bUser.user)
-})
-  }
 
 
 
