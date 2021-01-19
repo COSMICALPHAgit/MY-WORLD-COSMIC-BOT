f229bedc673aba16c902fb266de4c15a3f84eac8
COSMICALPHAgit <sudesh.kumarh2@gmail.com>
2021-01-19 11:02:01 +0530

    fedsg
    fgh

--- index.js
+++ index.js
@@ -64,9 +64,6 @@ client.on('message', message => {
   }else if (command === 'clear') {
     message.delete();
     client.commands.get('clear').execute(message, args);
-  }else if (command === 'warn') {
-    message.delete();
-    client.commands.get('warn').execute(message, args);
   }
 
 
