175df9d7980aff22a3788c6c8786643eafb6c37f
COSMICALPHAgit <sudesh.kumarh2@gmail.com>
2021-01-20 09:48:42 +0530

    ytjy7uk
    yukjyuik

---
+++ commands/unban.js
@@ -0,0 +1,24 @@
+const Discord = require(`discord.js`);
+
+module.exports.run = async(client, message, args) => {
+
+let User = args[0];
+let Reason = args.slice(1).join(` `);
+if (!User) return message.reply(`Who are we unbanning?`);
+if (!Reason) Reason = `Unbanned by ${message.author.tag}`
+
+message.guild.fetchBans().then(bans => {
+if (bans.some(u => User.includes(u.username))) {
+let user = bans.find(user.username === User);
+
+message.guild.unban(user.id, Reason);
+}
+else if (bans.some(u => User.includes(u.id))) {
+
+message.guild.unban(User, Reason);
+}
+else {
+return message.reply(`This person is not banned`);
+}
+});
+}
