445738accbb5af58a29448a6626597f0b019014c
COSMICALPHAgit <sudesh.kumarh2@gmail.com>
2021-01-20 11:01:04 +0530

    wqesd
    qwde

--- commands/ban.js
+++ commands/ban.js
@@ -1,22 +1,22 @@
-module.exports = {
-    name: 'ban',
-    description: "This command ban a member!",                      //created by COSMIC ALPHA  (OSMI(_ALPHA#1010
-    execute(message, args) {
+//module.exports = {
+    //name: 'ban',
+    //description: "This command ban a member!",                      //created by COSMIC ALPHA  (OSMI(_ALPHA#1010
+    //execute(message, args) {
+
+//
+        //const member = message.mentions.users.first();
+        //if (member) {
+          //  if (message.member.roles.cache.has('776786219145232406')) {
+             //   const memberTarget = message.guild.members.cache.get(member.id);
+           //     memberTarget.ban();
+             //   message.channel.send("User has been banned");
+           // } else {
 
 
-        const member = message.mentions.users.first();
-        if (member) {
-            if (message.member.roles.cache.has('776786219145232406')) {
-                const memberTarget = message.guild.members.cache.get(member.id);
-                memberTarget.ban();
-                message.channel.send("User has been banned");
-            } else {
 
-
-
-                message.channel.send('you dont have the right Permission from the server owner');
-
-            }
-        }
-    }
-}
+               // message.channel.send('you dont have the right Permission from the server owner');
+//
+          //  }
+       // }
+    //}
+//}
--- index.js
+++ index.js
@@ -58,10 +58,6 @@ client.on('message', message => {
     message.channel.send(text);
   } else if (command === 'kick') {
     client.commands.get('kick').execute(message, args);
-
-  } else if (command === 'ban') {
-    client.commands.get('ban').execute(message, args);
-
   }else if (command === 'off') {
     message.delete();
     client.commands.get('off').execute(message, args, Discord);
@@ -79,6 +75,37 @@ client.on('message', message => {
 
 
 
+let messageArray = message.content.split(" ")
+let args = messageArray.slice(1);
+
+let cmd = messageArray[0];
+
+if(cmd === "!ban") {
+    let toBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]);
+
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You need permissions!") 
+    if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("Bot need permissions!") 
+
+    const reason = args[1] || "There was no reason!";
+
+    toBan.ban({
+        reason: reason
+    })
+    message.channel.send(`${toBan} has been banned from the server!\nReason: ${reason}`)
+}
+
+if(cmd === "!unban") {
+    let toBan = await bot.users.fetch(args[0])
+
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You need permissions!") 
+    if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("Bot need permissions!") 
+
+    const reason = args[1] || "There was no reason!";
+
+    message.guild.members.unban(toBan, reason)
+
+    message.channel.send(`${toBan} has been unbanned from the server!`)
+}
 
 
 
